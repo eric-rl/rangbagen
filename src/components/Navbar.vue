b4aba5ae9e623aa32ad9e635548658340dd105ea
Eric Rasmusson Lindström <eric.rl@me.com>
2019-08-21 18:07:51 +0200

    Navbar mobile

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -20,7 +20,35 @@
       </nav>
     </div>
     <div class="nav-mobile">
-      
+      <v-sheet min-height="400" class="overflow-hidden">
+        <v-container class="fill-height">
+          <v-row align="center" justify="center">
+            <v-btn color="pink" dark @click.stop="drawer = !drawer">Toggle</v-btn>
+          </v-row>
+        </v-container>
+
+        <v-navigation-drawer fixed v-model="drawer" absolute temporary>
+          <v-list-item>
+            <v-list-item-avatar>
+              <v-img src="https://randomuser.me/api/portraits/men/78.jpg"></v-img>
+            </v-list-item-avatar>
+
+            <v-list-item-content>
+              <v-list-item-title>John Leider</v-list-item-title>
+            </v-list-item-content>
+          </v-list-item>
+
+          <v-divider></v-divider>
+
+          <v-list dense>
+            <v-list-item v-for="item in items" :key="item.title" link>
+              <v-list-item-content>
+                <v-list-item-title>{{ item.title }}</v-list-item-title>
+              </v-list-item-content>
+            </v-list-item>
+          </v-list>
+        </v-navigation-drawer>
+      </v-sheet>
     </div>
   </div>
 </template>
@@ -28,9 +56,16 @@
 <script>
 export default {
   name: "Navbar",
-  components: {
-
+  data() {
+    return {
+      drawer: null,
+      items: [
+        { title: "Home",  },
+        { title: "About",  }
+      ]
+    };
   },
+  components: {},
   methods: {
     handleScroll(event) {
       let nav = document.querySelector("#navbar");
@@ -55,75 +90,81 @@ export default {
 </script>
 
 <style>
-nav {
-  position: fixed;
-  height: 5em;
-  width: 100%;
-  background-color: transparent;
-  box-sizing: border-box;
-  padding: 12px 25px;
-  transition: background-color 0.3s ease-in-out;
-  z-index: 999;
-  color: whitesmoke;
-  display: flex !important;
-}
-
-nav.scroll {
-  background-color: rgb(223, 215, 225);
-}
-ul > li > a {
-  cursor: pointer;
-}
-ul.scroll > li > a {
-  color: black;
-}
-
-ul.scroll > li > a:after {
-  background: black;
-}
-
-ul {
-  padding: 0;
-  list-style: none;
-  display: flex;
-  width: 100%;
-  justify-content: flex-end;
-  align-items: center;
-}
-li {
-  display: table-cell;
-  position: relative;
-  margin-left: 30px;
-}
-a {
-  color: #fff;
-  text-transform: uppercase;
-  text-decoration: none;
-  letter-spacing: 0.15em;
-  display: inline-block;
-  padding: 5px 20px;
-  position: relative;
-}
-a:after {
-  background: none repeat scroll 0 0 transparent;
-  bottom: 0;
-  content: "";
-  display: block;
-  height: 2px;
-  left: 50%;
-  position: absolute;
-  background: #fff;
-  transition: width 0.3s ease 0s, left 0.3s ease 0s;
-  width: 0;
-}
-a:hover:after {
-  width: 100%;
-  left: 0;
-}
-
 @media only screen and (max-width: 960px) {
   .nav-desktop {
     display: none;
+  }
+}
+
+@media only screen and (min-width: 960px) {
+  .nav-mobile {
+    display: none;
+    z-index: 99;
+  }
+  nav {
+    position: fixed;
+    height: 5em;
+    width: 100%;
+    background-color: transparent;
+    box-sizing: border-box;
+    padding: 12px 25px;
+    transition: background-color 0.3s ease-in-out;
+    z-index: 999;
+    color: whitesmoke;
+    display: flex !important;
+  }
+
+  nav.scroll {
+    background-color: rgb(223, 215, 225);
+  }
+  ul > li > a {
+    cursor: pointer;
+  }
+  ul.scroll > li > a {
+    color: black;
+  }
+
+  ul.scroll > li > a:after {
+    background: black;
+  }
+
+  ul {
+    padding: 0;
+    list-style: none;
+    display: flex;
+    width: 100%;
+    justify-content: flex-end;
+    align-items: center;
+  }
+  li {
+    display: table-cell;
+    position: relative;
+    margin-left: 30px;
+  }
+  a {
+    color: #fff;
+    text-transform: uppercase;
+    text-decoration: none;
+    letter-spacing: 0.15em;
+    display: inline-block;
+    padding: 5px 20px;
+    position: relative;
+  }
+  a:after {
+    background: none repeat scroll 0 0 transparent;
+    bottom: 0;
+    content: "";
+    display: block;
+    height: 2px;
+    left: 50%;
+    position: absolute;
+    background: #fff;
+    transition: width 0.3s ease 0s, left 0.3s ease 0s;
+    width: 0;
+  }
+  a:hover:after {
+    width: 100%;
+    left: 0;
   }
 }
 </style>
